66db843b34e23e7a407c2c8ee0a9fe95a57b9175
huyy04 <huyvudinh04@gmail.com>
2025-04-09 21:15:35 +0700

    init commit

---
+++ test1/main.js
@@ -0,0 +1,18 @@
+
+let startDay = parseInt(prompt("nhap thong tin ngay bat dau"));
+let endDay = parseInt(prompt("nhap thong tin ngay ket thuc"));
+
+let saveDays = "";
+
+for (startDay ; startDay <= endDay ; startDay++) {
+    if (startDay > endDay) {
+        break;
+    }
+    // luu lai cac ngay thoa man
+    saveDays = saveDays + ' , ' + startDay;
+
+}
+console.log(saveDays);
+
+
+
